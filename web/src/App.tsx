814a767df068a68ca2ead988e32c6900ea06c585
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2025-01-29 02:54:20 +0100

    init: shadcn (tailwind, postcss, autoprefixer, lucide, date-fns)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+
 function App() {
-  return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  );
+	const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+	return (
+		<>
+			<Button>Click me</Button>
+			<Calendar mode="single" selected={date} onSelect={setDate} className="rounded-md border" />
+		</>
+	);
 }
 
 export default App;
